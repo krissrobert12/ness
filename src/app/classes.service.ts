382076c724994f6af4c555fbd73f8202c821671d
Kriss Gardner <krissrobert12@gmail.com>
2021-03-06 09:58:03 +0200

    Fix format bug

--- src/app/classes.service.ts
+++ src/app/classes.service.ts
@@ -11,11 +11,7 @@ export class ClassesService {
 
   constructor(private http: HttpClient) {}
 
-<<<<<<< HEAD
   getClasses(): Observable<WizardClass[]> {
-=======
-  getHeroes(): Observable<WizardClass[]> {
->>>>>>> 68c8dadd0d6a3fe367a30d8a27780792b1fd4e4d
     return this.http.get<WizardClass[]>(this.classesUrl);
   }
 }
